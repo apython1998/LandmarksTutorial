f996717552427cc4897d7090910ddb3152046874
apython1998 <alex.python1998@gmail.com>
2021-04-24 13:24:28 -0700

    Initial Commit

---
+++ LandmarksTutorial/LandmarksTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksTutorialApp.swift
+//  LandmarksTutorial
+//
+//  Created by Alex Python on 4/24/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
